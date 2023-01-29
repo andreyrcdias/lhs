3c865b722849c0e118e751b4b860d11821db672b
andreyrcdias <andreycadimaa@gmail.com>
2023-01-29 09:43:34 -0300

    fix hs style write

--- merge.hs
+++ merge.hs
@@ -6,14 +6,14 @@ merge (x:xs) (y:ys) =
   then y:merge (x:xs) ys
   else x:merge xs (y:ys)
 
-mergesort :: [Int] -> [Int]
-mergesort [] = []
-mergesort [x] = [x]
-mergesort [x,y] = if x > y then [y,x] else [x,y]
-mergesort x =
+mergeSort :: [Int] -> [Int]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort [x,y] = if x > y then [y,x] else [x,y]
+mergeSort x =
   let p = div (length x) 2 in
-  merge (mergesort $ take p x) (mergesort $ drop p x)
+  merge (mergeSort $ take p x) (mergeSort $ drop p x)
 
 main :: IO ()
-main = do print (mergesort [5,9,1,8,4,7,6,3,2])
+main = do print (mergeSort [5,9,1,8,4,7,6,3,2])
 
