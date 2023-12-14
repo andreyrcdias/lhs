2609f9f4aeaeb93d4823f83a1a220360aa9daf52
andreyrcdias <andreycadimaa@gmail.com>
2023-12-14 01:22:43 -0300

    added smart fibonacci

--- fib.hs
+++ fib.hs
@@ -5,9 +5,20 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
+
+smartFib :: Int -> Int
+smartFib 0 = 0
+smartFib 1 = 1
+smartFib n = f !! n
+  where
+    f = 0 : 1 : [f !! (i - 1) + f !! (i - 2) | i <- [2 .. n]]
+
 main :: IO ()
 main = do
     args <- getArgs
     let n = read (head args) :: Int
     print (fib n)
+
+    -- smart fibonnaci
+    -- print (smartFib n)
 
